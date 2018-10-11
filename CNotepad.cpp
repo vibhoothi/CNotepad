f1588dea4019182d223a85bf33b24362c54932a9
Vibhoothi <vibhoothiiaanand@gmail.com>
2018-10-11 19:04:41 +0530

    gui: Add fancy options to change colors
    This commit introduces options to change the GUI of the app as a whole

--- CNotepad.cpp
+++ CNotepad.cpp
@@ -373,6 +373,427 @@ void main()
 	    outtextxy(60,225,"  ugly look.");
 
 	}
+	if((x>=165&&x<=194)&&(y>=51&&y<=58)&&(cl==1))
+
+	{
+	    flag=3;
+	    closegraph();
+	    n.draw();
+	    outtextxy(60,81,"OK.This option is to completly revamp the");
+	    outtextxy(60,90,"colors of notepad so to take the experience");
+            outtextxy(60,99,"to next level ");
+            outtextxy(60,108,"The colour codes are listed below:");
+            outtextxy(60,117,"BLACK 0");
+            outtextxy(60,126,"BLUE 1");
+            outtextxy(60,135,"GREEN 2");
+	    outtextxy(60,144,"CYAN 3");
+	    outtextxy(60,153,"RED 4 ");
+            outtextxy(60,162,"MAGENTA 5 ");
+            outtextxy(60,171,"BROWN 6");
+            outtextxy(60,180,"LIGHTGRAY 7");
+	    outtextxy(60,189,"DARKGRAY 8");
+	    outtextxy(60,198,"LIGHTBLUE 9");
+            outtextxy(60,207,"LIGHTGREEN 10");
+            outtextxy(60,216,"LIGHTCYAN 11 ");
+            outtextxy(60,225,"LIGHTRED 12");
+	    outtextxy(60,234,"LIGHTMAGENTA 13");
+	    outtextxy(60,243,"YELLOW 14");
+            outtextxy(60,252,"WHITE 15 ");
+	    outtextxy(60,261,"Ok now select where to colour change");
+            outtextxy(60,270,"A:Text Field");
+            outtextxy(60,279,"B:outline");
+            outtextxy(60,288,"C:Menu Bar");
+            outtextxy(60,297,"D:Pop up color");
+	    outtextxy(60,308,"Enter Location:");
+	    gotoxy(27,20);
+	    char loc;
+            cin>>loc;
+	    outtextxy(60,324,"Enter Colour code:");
+            gotoxy(27,21);
+            int code;
+            cin>>code;
+            if(loc=='a'||loc=='A')
+            switch(code)
+            
+	    {
+                case 1 :
+
+		{
+		    if(code==0)
+                    n.field=0;
+                    cout<<n.field;
+                    n.draw();
+
+                }
+		case 2 :
+                
+                {
+                    if(code==1)
+                    n.field=1;
+		    n.draw();
+
+                }
+                case 3 :
+
+                {
+                    if(code==2)
+		    n.field=2;
+		    n.draw();
+                }
+                case 4 :
+
+		{
+		    if(code==3)
+		    n.field=3;
+		    n.draw();
+                }
+                case 5 :
+                
+                {
+                    if(code==4)
+                    n.field=4;
+		    n.draw();
+		}
+                case 6 :
+                
+		{
+		    if(code==5)
+                    n.field=5;
+		    n.draw();
+		}
+                case 7 :
+                
+		{
+                    if(code==6)
+                    n.field=6;
+		    n.draw();
+                }
+                case 8 :
+
+		{
+                    if(code==7)
+		    n.field=7;
+		    n.draw();
+		}
+                case 9 :
+                
+                {
+		    if(code==8)
+                    n.field=8;
+		    n.draw();
+		}
+		case 10 :
+
+                {
+		    if(code==9)
+                    n.field=9;
+		    n.draw();
+                }
+                case 11 :
+                
+		{
+                    if(code==10)
+                    n.field=10;
+		    n.draw();
+		}
+		case 12 :
+                
+                {
+                    if(code==11)
+		    n.field=11;
+		    n.draw();
+                }
+                case 13 :
+                
+                {
+                    if(code==12)
+		    n.field=12;
+		    n.draw();
+		}
+		case 14 :
+                
+                {
+		    if(code==13)
+                    n.field=13;
+		    n.draw();
+                }
+                case 15 :
+
+                {
+                    if(code==14)
+		    n.field=14;
+		    n.draw();
+                }
+                case 16 :
+                
+                {
+                    if(code==15)
+		    n.field=15;
+		    n.draw();
+		}
+            }
+            if(loc=='b'||loc=='B')
+            switch(code)
+
+	    {
+                case 1 :
+                { if(code==0)
+                    n.outline=0;
+                    cout<<n.outline;
+		    n.draw();
+                    break;
+                }
+                case 2 :
+                { if(code==1)
+                    n.outline=1;
+                    n.draw();
+		}
+		case 3 :
+                { if(code==2)
+                    n.outline=2;
+		    n.draw();
+                }
+                case 4 :
+                { if(code==3)
+                    n.outline=3;
+                    n.draw();
+                }
+                case 5 :
+                { if(code==4)
+		    n.outline=4;
+		    n.draw();
+		}
+                case 6 :
+                { if(code==5)
+                    n.outline=5;
+                    n.draw();
+		}
+                case 7 :
+                { if(code==6)
+                    n.outline=6;
+                    n.draw();
+                }
+		case 8 :
+		{ if(code==7)
+                    n.outline=7;
+                    n.draw();
+                }
+                case 9 :
+                { if(code==8)
+                    n.outline=8;
+                    n.draw();
+                }
+                case 10 :
+		{ if(code==9)
+                    n.outline=9;
+		    n.draw();
+		}
+                case 11 :
+                { if(code==10)
+                    n.outline=10;
+                    n.draw();
+                }
+                case 12 :
+                { if(code==11)
+		    n.outline=11;
+                    n.draw();
+                }
+                case 13 :
+		{ if(code==12)
+		    n.outline=12;
+                    n.draw();
+                }
+                case 14 :
+                { if(code==13)
+		    n.outline=13;
+		    n.draw();
+                }
+                case 15 :
+                { if(code==14)
+                    n.outline=14;
+                    n.draw();
+		}
+		case 16 :
+                { if(code==15)
+                    n.outline=15;
+                    n.draw();
+		}
+            }
+            if(loc=='c'||loc=='C')
+            switch(code)
+            
+            {
+                case 1 :
+		{ if(code==0)
+		    n.menu=0;
+		    cout<<n.menu;
+                    n.draw();
+		}
+                case 2 :
+                { if(code==1)
+                    n.menu=1;
+                    n.draw();
+                }
+                case 3 :
+                { if(code==2)
+                    n.menu=2;
+                    n.draw();
+		}
+		case 4 :
+                { if(code==3)
+                    n.menu=3;
+                    n.draw();
+		}
+                case 5 :
+                { if(code==4)
+                    n.menu=4;
+                    n.draw();
+                }
+                case 6 :
+		{ if(code==5)
+		    n.menu=5;
+		    n.draw();
+                }
+                case 7 :
+                { if(code==6)
+                    n.menu=6;
+                    n.draw();
+                }
+                case 8 :
+                { if(code==7)
+		    n.menu=7;
+		    n.draw();
+                }
+		case 9 :
+		{ if(code==8)
+                    n.menu=8;
+                    n.draw();
+                }
+                case 10 :
+                { if(code==9)
+                    n.menu=9;
+		    n.draw();
+                }
+                case 11 :
+                { if(code==10)
+                    n.menu=10;
+		    n.draw();
+		}
+                case 12 :
+                { if(code==11)
+		    n.menu=11;
+                    n.draw();
+		}
+                case 13 :
+                { if(code==12)
+                    n.menu=12;
+                    n.draw();
+                }
+                case 14 :
+		{ if(code==13)
+		    n.menu=13;
+                    n.draw();
+                }
+		case 15 :
+                { if(code==14)
+                    n.menu=14;
+                    n.draw();
+                }
+                case 16 :
+		{ if(code==15)
+                    n.menu=15;
+                    n.draw();
+		}
+
+	    }
+            if(loc=='d'||loc=='D')
+	    switch(code)
+            
+            {
+                case 1 :
+                { if(code==0)
+                    n.popup=0;
+                    cout<<n.popup;
+                    n.draw();
+                }
+		case 2 :
+		{ if(code==1)
+		    n.popup=1;
+		    n.draw();
+                }
+                case 3 :
+                { if(code==2)
+                    n.popup=2;
+                    n.draw();
+                }
+                case 4 :
+		{ if(code==3)
+                    n.popup=3;
+		    n.draw();
+		}
+                case 5 :
+                { if(code==4)
+                    n.popup=4;
+                    n.draw();
+                }
+                case 6 :
+                { if(code==5)
+		    n.popup=5;
+                    n.draw();
+		}
+                case 7 :
+		{ if(code==6)
+		    n.popup=6;
+                    n.draw();
+                }
+                case 8 :
+                { if(code==7)
+                    n.popup=7;
+		    n.draw();
+                }
+		case 9 :
+                { if(code==8)
+                    n.popup=8;
+                    n.draw();
+		}
+		case 10 :
+		{ if(code==9)
+                    n.popup=9;
+                    n.draw();
+		}
+                case 11 :
+		{ if(code==10)
+                    n.popup=10;
+                    n.draw();
+                }
+                case 12 :
+                { if(code==11)
+		    n.popup=11;
+		    n.draw();
+                }
+		case 13 :
+                { if(code==12)
+		    n.popup=12;
+                    n.draw();
+                }
+		case 14 :
+                { if(code==13)
+                    n.popup=13;
+                    n.draw();
+                }
+		case 15 :
+		{ if(code==14)
+                    n.popup=14;
+		    n.draw();
+                }
+                case 16 :
+                { if(code==15)
+                    n.popup=15;
+                    n.draw();
+                }
+                
+            }
+	}
 
   }
 
